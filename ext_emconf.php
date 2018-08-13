6cebf16ef9af03e9d57b42c8df434b19b75d42c8
Richard Haeser <richard@maxserv.com>
2018-08-13 20:50:33 +0200

    [TASK] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,15 @@
+<?php
+$EM_CONF['seo_demo_extension'] = [
+    'title' => 'TYPO3 CMS SEO features - Demo extension',
+    'description' => '',
+    'category' => 'fe',
+    'state' => 'stable',
+    'author' => 'Richard Haeser',
+    'author_email' => 'richardhaeser@gmail.com',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [],
+        'conflicts' => [],
+        'suggests' => []
+    ],
+];
